2e76537328c4cfb8e4eb2e4915677ae7a0d15857
Harry Joseph <hjoseph777@gmail.com>
2025-12-06 17:53:33 -0500

    Simplify Vercel deployment - use standard Django WSGI

--- AHBookStore/wsgi.py
+++ AHBookStore/wsgi.py
@@ -1,4 +1,9 @@
 import os
+import sys
+from pathlib import Path
+
+# Add the parent directory to the Python path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 from django.core.wsgi import get_wsgi_application
 
